d8ea29befbf4140a83a79513153000b8fd7118c6
kishenkoilya <illad08@gmail.com>
2025-03-27 10:47:57 +0300

    fixed path in engine_settings and uncommented one method in grammar

--- analysis/api-scan/fuzzing/grammar_fuzz.py
+++ analysis/api-scan/fuzzing/grammar_fuzz.py
@@ -23,23 +23,23 @@ requestId=""
 req_collection.add_request(request)
 
 # Endpoint: /books/v1, method: Get
-# request = requests.Request([
-#     primitives.restler_static_string("GET "),
-#     primitives.restler_basepath(""),
-#     primitives.restler_static_string("/"),
-#     primitives.restler_static_string("books"),
-#     primitives.restler_static_string("/"),
-#     primitives.restler_static_string("v1"),
-#     primitives.restler_static_string(" HTTP/1.1\r\n"),
-#     primitives.restler_static_string("Accept: application/json\r\n"),
-#     primitives.restler_static_string("Host: \r\n"),
-#     primitives.restler_refreshable_authentication_token("authentication_token_tag"),
-#     primitives.restler_static_string("\r\n"),
+request = requests.Request([
+    primitives.restler_static_string("GET "),
+    primitives.restler_basepath(""),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string("books"),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string("v1"),
+    primitives.restler_static_string(" HTTP/1.1\r\n"),
+    primitives.restler_static_string("Accept: application/json\r\n"),
+    primitives.restler_static_string("Host: \r\n"),
+    primitives.restler_refreshable_authentication_token("authentication_token_tag"),
+    primitives.restler_static_string("\r\n"),
 
-# ],
-# requestId="/books/v1"
-# )
-# req_collection.add_request(request)
+],
+requestId="/books/v1"
+)
+req_collection.add_request(request)
 
 # Endpoint: /books/v1, method: Post
 request = requests.Request([
